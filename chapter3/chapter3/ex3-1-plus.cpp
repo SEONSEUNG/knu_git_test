2984e65996f3615426595a464449049b32b2ac22
SEONSEUNG <seonseung@kakao.com>
2025-03-20 14:49:17 +0900

    chapter3 examples is added

---
+++ chapter3/chapter3/ex3-1-plus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+class Circle {
+public:
+	double getArea();
+	int innerRadius, outerRadius;
+};
+
+double Circle::getArea() {
+
+	return (3.14 * outerRadius * outerRadius) - (3.14 * innerRadius * innerRadius);
+
+}
+
+int main() {
+
+	Circle donut_two;
+	cout << "Enter innerRadius >> ";
+	cin >> donut_two.innerRadius;
+	cout << "Enter outerRadius >> ";
+	cin >> donut_two.outerRadius;
+
+	double area = donut_two.getArea();
+
+	cout << "Area: " << area << endl;
+
+}
+*/
