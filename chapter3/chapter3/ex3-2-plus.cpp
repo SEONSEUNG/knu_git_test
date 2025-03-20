a2a894e8fd9ada6424118f08c47ff69aa3ad0ab9
SEONSEUNG <seonseung@kakao.com>
2025-03-20 16:34:52 +0900

    chapter3 examples complete

---
+++ chapter3/chapter3/ex3-2-plus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+class Rectangle {
+public:
+	int width, height;
+	int thickness;
+	int getArea();
+};
+
+int Rectangle::getArea() {
+	return width * height * thickness;
+}
+
+int main() {
+
+	Rectangle rect;
+	
+	cout << "Enter width >> ";
+	cin >> rect.width;
+	cout << "Enter height >> ";
+	cin >> rect.height;
+	cout << "Enter thickness >> ";
+	cin >> rect.thickness;
+
+	cout << "Area: " << rect.getArea() << endl;
+
+}
+*/
